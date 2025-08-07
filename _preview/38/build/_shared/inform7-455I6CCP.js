11b3bf5c5dc5d1fa3d174bc4ca15840ec40a9f55
jthielen <3460034+jthielen@users.noreply.github.com>
2025-08-07 17:28:57 +0000

    Delete preview for pull request \#38

--- _preview/38/build/_shared/inform7-455I6CCP.js
+++
@@ -1,1 +0,0 @@
-import{a}from"/atmos-modeling-cookbook/_preview/38/build/_shared/chunk-GNTAXZUX.js";import"/atmos-modeling-cookbook/_preview/38/build/_shared/chunk-2NH4LW52.js";export default a();
--- _preview/38/build/_shared/neon-TUR2OW3Q.js
+++
@@ -1,1 +0,0 @@
-import{a}from"/atmos-modeling-cookbook/_preview/38/build/_shared/chunk-2LT6OJ7Z.js";import"/atmos-modeling-cookbook/_preview/38/build/_shared/chunk-2NH4LW52.js";export default a();
